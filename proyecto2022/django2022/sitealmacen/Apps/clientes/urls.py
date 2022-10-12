96d95458ca7ffcf2e4dca86cd121dd6d379d4924
jecar7 <jdcardona@uniguajira.edu.co>
2022-10-12 18:14:22 -0500

    4

--- proyecto2022/django2022/sitealmacen/Apps/clientes/urls.py
+++ proyecto2022/django2022/sitealmacen/Apps/clientes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from Apps.clientes.views import home
+from Apps.clientes.views import ClienteList, ClienteDetail
 
+app_name = "clientes"
 urlpatterns = [
-    path('inicio/', home, name= 'home'),
+    path('', ClienteList.as_view()),
+    path('<int:pk>', ClienteDetail.as_view()),
 ]
